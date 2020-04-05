1ad8fea8c2afcb5f4a751ac1b139fc1d96171262
Ardian F Firmansyah <ardianfirmansyah123@gmail.com>
2020-04-05 13:29:00 +0700

    add number pagination & add image in product detail

--- src/product/views.py
+++ src/product/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, ProductImages
+from django.core.paginator import Paginator
 # Create your views here.
 
 
@@ -7,6 +8,11 @@ from .models import Product
 def productlist(request):
     productlist = Product.objects.all()
     template = 'Product/product_list.html'
+
+    paginator = Paginator(productlist, 1) # Show 25 contacts per page.
+    page = request.GET.get('page')
+    productlist = paginator.get_page(page)
+
     context = {'product_list' : productlist}
     return render(request, template, context)
 
@@ -16,6 +22,7 @@ def productlist(request):
 def productdetail(request, product_slug):
     print(product_slug)
     productdetail = Product.objects.get(slug=product_slug)
+    productimages = ProductImages.objects.filter(product = productdetail)
     template = 'Product/product_detail.html'
-    context = {'product_detail' : productdetail}
+    context = {'product_detail' : productdetail, 'product_images': productimages}
     return render(request, template, context)
